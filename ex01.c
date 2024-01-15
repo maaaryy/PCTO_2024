3da59eddbc0a0d9cf4764f731e45de294ff53388
maaaryy <156683143+maaaryy@users.noreply.github.com>
2024-01-15 15:19:12 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("ciao <3");
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x = 5;
+    float y = 2.5;
+    long z = 'ciao a tutti';
+ printf ( "%d\n", x );
+ printf ( "%f\n", y );
+ printf ( "%lo\n", z );
+ return 0;
+}
