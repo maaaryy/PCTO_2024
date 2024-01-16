09c53c67afada9308927f829ff951ba8218f2cbb
maaaryy <156683143+maaaryy@users.noreply.github.com>
2024-01-16 23:14:28 +0000

    ciaociao

---
+++ ex23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b = 1969;
+    
+    printf("inserisci anno di nascita:\n");
+    scanf("%d", &a);
+
+    if(a == b){
+        printf("è nato l'anno  in cui l'uomo andò sulla luna\n");
+    }else if(a!=b && a>b){
+        printf("non è nato l'anno in cui l'uomo andò sulla luna ma: %d\n anni dopo", a-b );
+    }else if(a!=b && a<b){
+        printf("non è nato l'anno in  cui l'uomo andò sulla luna ma: %d\n anni prima", b-a );
+    }
+     return(0);
+}
